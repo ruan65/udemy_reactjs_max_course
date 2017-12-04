f85f978d79716409fde7286e8e2c9b0da65ecf61
ruan65 <ruan65@gmail.com>
2017-12-04 11:44:44 +0300

    ubuntu_work

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css'
 
 class App extends Component {
     render() {
-        return (
-            <div className="App">
-                <h1>Hi, I'm a React App</h1>
-            </div>
-        )
+        // return (
+        //     <div className="App">
+        //         <h1>Hi, I'm a React App</h1>
+        //     </div>
+        // )
 
-        // return React.createElement('div', {className: 'App'},
-        //     React.createElement('h1', null, 'Hi, I\'m Andreyka!!!'))
+        return React.createElement('div', {className: 'App'},
+            React.createElement('h1', null, 'Hi, I\'m Andreyka!!!'))
     }
 }
 
