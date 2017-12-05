cd8a4eec8d17754946d6d73b2cd14cf5e423f471
ruan65 <ruan65@gmail.com>
2017-12-05 20:22:15 +0300

    change state concept

--- src/App.js
+++ src/App.js
@@ -5,15 +5,41 @@ import Person from './Person/Person'
 import Boss from './Person/Boss'
 
 class App extends Component {
+
+    state = {
+        persons: [
+            {name: 'Zina', age: 34},
+            {name: 'Zinaida', age: 33},
+            {name: 'Zinatulla', age: 32},
+            {name: 'Zinacrone', age: 31}
+        ]
+    }
+
+    switchNameHandler = () => {
+
+        // console.log("Clicked.......")
+
+        this.setState({persons: [
+                {name: 'Grisha', age: 34},
+                {name: 'Zinaida', age: 33},
+                {name: 'Zinatulla', age: 32},
+                {name: 'Zinacrone', age: 3177}
+            ] })
+    }
+
     render() {
         return (
             <div className="App">
-               <h1>Hi, I'm a React App</h1>
-                <Boss power='strong enough' age='250'/>
-                <Person name='Andreyka' age='23'/>
-                <Person name='Fenia' age='123'/>
-                <Person name='Lola' age='73'/>
-                <Person name='Pete' age='13'/>
+
+                <h1>Hi, I'm a React App</h1>
+
+                <button onClick={this.switchNameHandler}>Switch name</button>
+
+                <Boss power='strong enough' age='250'>Behold!!!</Boss>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
 
             </div>
         )
--- src/Person/Boss.js
+++ src/Person/Boss.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react'
 class Boss extends Component {
 
     render() {
-        return <p>I'm Boss {this.props.power} my age is {this.props.age}</p>
+        return (
+            <div>
+                <p>{this.props.children}</p>
+                <p>I'm Boss {this.props.power} my age is {this.props.age}</p>
+            </div>
+        )
     }
 }
 
